cf984b41b9b280b2d4318a0e84440e0c63df9c0d
frang6 <garciapedrajas21@gmail.com>
2025-02-03 22:25:12 +0100

    creacion pagina principal

--- bazarOnline/src/App.vue
+++ bazarOnline/src/App.vue
@@ -3,10 +3,25 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  
-  <RouterView />
+  <div id="app">
+    <!-- Aquí se mostrará la vista según la ruta activa -->
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
+nav {
+  margin: 20px;
+}
 
+RouterLink {
+  margin-right: 20px;
+  text-decoration: none;
+  color: #42b983;
+}
+
+RouterLink:hover {
+  text-decoration: underline;
+}
 </style>
+
