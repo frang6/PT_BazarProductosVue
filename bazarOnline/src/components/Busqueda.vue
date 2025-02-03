0019a29a2fdbb92328a06d89412dc1a1aa93a5f0
frang6 <garciapedrajas21@gmail.com>
2025-02-03 12:20:27 +0100

    ajuste de estilo

--- bazarOnline/src/components/Busqueda.vue
+++ bazarOnline/src/components/Busqueda.vue
@@ -3,7 +3,9 @@
         <img src="../assets/img/logo.png" alt="Logo" />
         <h2>Bazar Online Fran Garcia</h2>
         <form @submit.prevent="buscar">
-            <input type="text" v-model="busqueda" placeholder="Buscar..." class="input"/>
+            <div class="InputContainer">
+                <input type="text" v-model="busqueda" placeholder="Buscar..." class="input"/>
+            </div>
             <Boton label="Buscar" type="submit" class="boton" />
         </form>
     </main>
@@ -39,19 +41,45 @@ h2{
 
 }
 
-.input{
-    width: 300px;
-    height: 50px;
-    border-radius: 10px;
-    border: 1px solid #ccc;
-    padding: 10px;
-    font-size: 1.2em;
+.InputContainer {
+  width: 210px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(to bottom,rgb(240, 136, 122),rgb(235, 208, 60));
+  border-radius: 30px;
+  overflow: hidden;
+  cursor: pointer;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.075);
 }
 
+.input {
+  width: 200px;
+  height: 40px;
+  border: none;
+  outline: none;
+  caret-color: rgb(255, 81, 0);
+  background-color: rgb(255, 255, 255);
+  border-radius: 30px;
+  padding-left: 15px;
+  letter-spacing: 0.8px;
+  color: rgb(19, 19, 19);
+  font-size: 13.4px;
+}
+
+
 .boton{
-    margin: 10px;
+    margin: 20px;
     height: 50px;
-    margin-left: 25%;
+    margin-left: 18%;
+    box-shadow: 0 10px 15px rgba(0,0,0,0.3);
+}
+
+.boton:hover{
+    scale: 1.1;
+    transition: 0.5s;
+    background: linear-gradient(to bottom,rgb(240, 136, 122),rgb(235, 208, 60));
 }
 
 
